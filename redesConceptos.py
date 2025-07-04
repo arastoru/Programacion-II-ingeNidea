2a5fe06b3a92093136a127539c029520421a2668
arastoru <andresalmanza123456@gmail.com>
2025-07-04 10:33:19 -0400

    Update redesConceptos.py

--- redesConceptos.py
+++ redesConceptos.py
@@ -39,3 +39,19 @@ Link = direccion ip URL
 diferencia switch router  CAPA CENTRAL ES RED trabaja router mediante direccion Ip router entiende , switch NO, swich solo entiende MAC
 MODELO TCP/IP 4 CAPAS 
 MODELO OSI 7 CAPAS
+*NOTA aprenderse las capas
+*configurar en cisco packet tracver dos SWITCH 2960 a distintias pc/ primera con 4, segunda con 3
+EN LA PAG DE CISCO
+MODULO 2
+MODULO 3
+MODULO 4
+MODULO 6 CAPA 2
+MODULO 7 CAPA 2
+MODULO 8 CAPA 3 
+MODULO 9 CApa 3
+MODULO 10
+MODULO 13 CAPA 3
+MODULO 14 CAPA 4
+MODULO 15 CAPA 5/6/7
+MODULO 16 
+MODULO 
