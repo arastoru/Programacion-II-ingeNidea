43cea5810794ac9458d820d115f964ca057ebe1e
arastoru <andresalmanza123456@gmail.com>
2025-07-04 07:46:35 -0400

    Create redesConceptos.py

---
+++ redesConceptos.py
@@ -0,0 +1,41 @@
+"""ISP
+internet
+satelite
+antena
+fibra optica
+modem
+router
+Servidores
+switches
+wifi
+Proveedor de servicio
+DSL
+Modem
+Modem isp
+WAN/LAN
+DHCP = distribuir conexiones ip
+DNS = 
+Link = direccion ip URL 
+  internet = red de redes (REDES) (REDES) (REDES)
+  red = dispositivos interconectados
+  Dispositivo intermedio
+  --Modem, router, switch
+  Dispositivo final
+  --- pc, telf, impresora
+  *host
+  *cliente
+  ADSL
+  -------------
+  DATOS
+  VOZ
+  ------------
+  HFC
+  -------
+  DATOS 
+  
+#IMPORTANTE VER DIAPOSITIVAS  EN LA PLATAFORMA/CISCO PACKET TRACER = hacer enfasis en WAN LAN al estudiar
+#que es una RED (diapositiva) /// sacado desde el (Forouzan 2007)
+7 capas Cisco  // capa fisica hasta capa usuario
+diferencia switch router  CAPA CENTRAL ES RED trabaja router mediante direccion Ip router entiende , switch NO, swich solo entiende MAC
+MODELO TCP/IP 4 CAPAS 
+MODELO OSI 7 CAPAS
